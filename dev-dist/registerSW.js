d9fd91ce8ea51bfe14138e9efed7990797386c5b
Savvypool <125363786+Savvypool@users.noreply.github.com>
2024-01-09 18:23:36 +0530

    made the website to pwa

---
+++ dev-dist/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) navigator.serviceWorker.register('/exaine-tasks/dev-sw.js?dev-sw', { scope: '/exaine-tasks/', type: 'classic' })
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,49 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        "name": "Exaine-tasks",
+        "short_name": "Exaine",
+        "description": "Tasks made easy",
+        "start_url": "/",
+        "display": "standalone",
+        "background_color": "#ffffff",
+        "theme_color": "#000000",
+        "icons": [
+          {
+            "src": "./exaine.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "./exaine.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          }
+        ],
+        "splash_pages": null,
+        "scope": "/",
+        "dir": "auto",
+        "lang": "en-US",
+        "prefer_related_applications": false,
+        "related_applications": [],
+        "serviceworker": {
+          "src": "/sw.js",
+          "scope": "/",
+          "type": "module"
+        }
+      }
+      ,
+      devOptions: {
+        enabled: true
+      }
+    })
+  ],
   base: "/exaine-tasks/"
 })
